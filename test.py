285e3186ef5df9aaf122fc9435a539579b7a3c57
Anurag A <anurag2798@gmail.com>
2019-11-04 19:00:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,15 @@ from datetime import datetime
 
 def motiondetect(a):
     config = {
-    "apiKey": "AIzaSyASNC2ZMvG8-ht1fxwafEBwAy9g0kJAX-o",
-    "authDomain": "etrack-41282.firebaseapp.com",
-    "databaseURL": "https://etrack-41282.firebaseio.com",
-    "storageBucket": "etrack-41282.appspot.com"
+    "apiKey": "AIzaSyCxPQOWBwRwIGCm8nKi6vU_SPNwHtX187k",
+    "authDomain": "facedb-6a754.firebaseapp.com",
+    "databaseURL": "https://facedb-6a754.firebaseio.com",
+    "storageBucket": "facedb-6a754.appspot.com"
     }
     firebase = pyrebase.initialize_app(config)
     db = firebase.database()
     data = {"Timestamp : ": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),"name": a}
-    db.child("FNET").push(data)
+    db.child("FaceDB").push(data)
+
+
+
