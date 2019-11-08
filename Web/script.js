ef2403c2913766b31f44b5d7f9979950ed734eb9
Anurag A <anurag2798@gmail.com>
2019-11-08 14:22:04 +0530

    Update script.js

--- Web/script.js
+++ Web/script.js
@@ -9,14 +9,42 @@ starCountRef.once('value', function(snapshot) {
     var uddhavData = data["Uddhav"];
     document.getElementById("name").innerHTML = uddhavData.Name;
     document.getElementById("ts").innerHTML = uddhavData.TimeStamp;
+    
+    var anuragData = data["Anurag"];
+  document.getElementById("Aname").innerHTML = anuragData.Name;
+  document.getElementById("Ats").innerHTML = anuragData.TimeStamp;
+    
+  var nigelData = data["nigel"];
+  document.getElementById("Nname").innerHTML = nigelData.Name;
+  document.getElementById("Nts").innerHTML = nigelData.TimeStamp;
+  
+  var prithviData = data["prithvi"];
+  document.getElementById("Pname").innerHTML = prithviData.Name;
+  document.getElementById("Pts").innerHTML = prithviData.TimeStamp;
+  
+  var purveshData = data["purvesh"];
+  document.getElementById("Puname").innerHTML = purveshData.Name;
+  document.getElementById("Puts").innerHTML = purveshData.TimeStamp;
+
+  var pratikData = data["pratik"];
+  document.getElementById("Prname").innerHTML = pratikData.Name;
+  document.getElementById("Prts").innerHTML = pratikData.TimeStamp;
+
+  var ZData = data["zia"];
+  document.getElementById("Zname").innerHTML = ZData.Name;
+  document.getElementById("Zts").innerHTML = ZData.TimeStamp;
+
+  var SData = data["santhosh"];
+  document.getElementById("Sname").innerHTML = SData.Name;
+  document.getElementById("Sts").innerHTML = SData.TimeStamp;
 });
 
 starCountRef.on('value', function(snapshot) {
 //   console.log(snapshot.val());
   data = snapshot.val();
-  //var uddhavData = data["Uddhav"];
-  //document.getElementById("name").innerHTML = uddhavData.Name;
-  //document.getElementById("ts").innerHTML = uddhavData.TimeStamp;
+  var uddhavData = data["Uddhav"];
+  document.getElementById("name").innerHTML = uddhavData.Name;
+  document.getElementById("ts").innerHTML = uddhavData.TimeStamp;
 
   var anuragData = data["Anurag"];
   document.getElementById("Aname").innerHTML = anuragData.Name;
